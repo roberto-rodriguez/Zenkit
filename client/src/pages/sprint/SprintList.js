8a5aef8750aaaed6cd8033019f91ce6cd481d3ec
eblancod <eduar_p87@hotmail.com>
2019-08-14 22:32:35 -0400

    Button AddSprint

--- client/src/pages/sprint/SprintList.js
+++ client/src/pages/sprint/SprintList.js
@@ -6,10 +6,10 @@ import Page from "../common/cmp/Page";
 import * as sprintActions from "./sprint.actions";
 import { connect } from "react-redux";
 import SprintBoardHeader from "./cmp/SprintBoardHeader";
-import { Button, Icon , Modal} from "react-materialize";
+import { Button, Icon, Modal } from "react-materialize";
+import AddAction from "./AddAction";
 
 class SprintList extends Component {
-
   componentDidMount() {
     var { list, listSprints } = this.props;
 
@@ -21,7 +21,6 @@ class SprintList extends Component {
   render() {
     let add = 1;
     let sprintList = this.props.sprintList || [];
-    console.log(this.state.modal);
     return (
       <Page>
         <div
@@ -32,17 +31,34 @@ class SprintList extends Component {
           }}
         >
           <div className="right">
-            <Button waves="light" className="lighten-4">
-              Add Sprint <Icon>add</Icon>
+            <Button
+              waves="light"
+              href="#modal1"
+              className="lighten-4 modal-trigger"
+            >
+              Add Sprint <Icon className="center">add</Icon>
             </Button>
+
+            <Modal header="Add Sprint" id="modal1" actions>
+
+              <AddAction />
+              <div className="modal-footer">
+              <Button className="modal-close" waves="light">
+                  INSERT<Icon>child_care</Icon>
+                </Button>{' '}
+                <Button className="modal-close" waves="light">
+                  Close<Icon>directions_bike</Icon>
+                </Button>
+              </div>
+            </Modal>
           </div>
           <br />
           <br />
           {sprintList.map((s, i) => (
-            <SprintBoardHeader sprint={s} addbut={add} /> 
+            <SprintBoardHeader sprint={s} addbut={add} />
           ))}
         </div>
-       </Page>
+      </Page>
     );
   }
 }
